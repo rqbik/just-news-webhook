25e8a9a5d6920745b37918e70455fc8a78d9663a
rqbik <flexingallday@mail.ru>
2020-12-19 15:12:59 +0200

    Authorization

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,30 @@
 import { NowRequest, NowResponse } from '@vercel/node';
 
+interface GitlabJobEvent {
+  build_name: 'build' | 'deploy';
+  build_status: 'failed' | 'success';
+  repository: {
+    name: string;
+  };
+}
+
 export default (request: NowRequest, response: NowResponse) => {
+  if (request.headers['x-gitlab-event'] !== process.env.GITLAB_TOKEN)
+    return response.status(401).json({ error: 'Unathorized' });
+
+  const {
+    repository,
+    build_name,
+    build_status,
+  } = request.body as GitlabJobEvent;
+
+  if (
+    repository.name !== 'JustWebsite' ||
+    build_name !== 'deploy' ||
+    build_status === 'failed'
+  )
+    return response.status(404).json({ error: 'Invalid webhook' });
+
   console.log(request.body, request.headers, request.rawHeaders);
   response.status(200).json({ hello: 'world!' });
 };

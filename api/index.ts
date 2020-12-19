f12cc6bf774836c3cdf2e9e0cc779e99fb93a8e2
rqbik <flexingallday@mail.ru>
2020-12-19 14:55:04 +0200

    Initial commit

---
+++ api/index.ts
@@ -0,0 +1,5 @@
+import { NowRequest, NowResponse } from '@vercel/node';
+
+export default (request: NowRequest, response: NowResponse) => {
+  console.log(request.body, request.headers, request.rawHeaders);
+};

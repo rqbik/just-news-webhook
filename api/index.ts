c5e0e4bad9c97109ae89545fc8c2ef391895e66b
rqbik <flexingallday@mail.ru>
2020-12-19 15:00:18 +0200

    Return response

--- api/index.ts
+++ api/index.ts
@@ -2,4 +2,5 @@ import { NowRequest, NowResponse } from '@vercel/node';
 
 export default (request: NowRequest, response: NowResponse) => {
   console.log(request.body, request.headers, request.rawHeaders);
+  response.status(200).json({ hello: 'world!' });
 };
